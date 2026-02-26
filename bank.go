8ec974dbc27e4670fa4531538910901c64628096
Emine KIRKPINAR EMMEZ <kirkpinaremine@gmail.com>
2026-02-26 14:25:25 +0100

    Initial CLI banking app version

---
+++ bank.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	accountBalance := 1000.0
+
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1.Check Balance")
+	fmt.Println("2.Deposit money")
+	fmt.Println("3.Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("your choice: ")
+	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	} else if choice == 2 {
+		fmt.Println("How much do you want to deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+		fmt.Println("Balance updated! New amount:", accountBalance)
+	} else if choice == 3 {
+		fmt.Println("Withdrawal amount: ")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+		accountBalance -= withdrawalAmount
+		fmt.Println("Balance updated! New amount:", accountBalance)
+	}
+
+	fmt.Println("Your choice:", choice)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/didemre/bank-app
+
+go 1.25.0
